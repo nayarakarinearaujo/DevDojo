1fae6214e24099ecc6814c2a47c550191bde6a71
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-04-17 15:38:56 -0300

    Add files via upload

---
+++ src/MaratonaDojo/Aula07Arrays02.java
@@ -0,0 +1,18 @@
+package MaratonaDojo;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+
+        //byte, short, int,long, float, e double valor 0
+        //char, 'u000/' ''
+        //boolean false
+        //String null
+        //Se alterar não vai rodar
+
+        int [] idades = new int[3];
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
---
+++ src/MaratonaDojo/Aula07Arrays03.java
@@ -0,0 +1,15 @@
+package MaratonaDojo;
+
+public class Aula07Arrays03 {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[3];
+        int[] numero2 = {1, 2, 3, 4};
+        int[] numero3 = new int[]{5, 4, 3, 2, 1};
+
+        for (int i = 0; i < numero2.length; i++) {
+            System.out.println(numero3[i]);
+
+        }
+    }
+}
