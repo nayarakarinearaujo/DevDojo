28f8bfdea210588ecc02892dcbbd3d1513ded947
-Nayara <nayarakarine.araujo@gmail.com>
2024-06-05 16:45:10 -0300

    Aula dia 05/06/24

---
+++ src/MaratonaDojo/Aula02TiposPrimitivosStrings.java
@@ -0,0 +1,39 @@
+package MaratonaDojo;
+
+public class Aula02TiposPrimitivosStrings {
+
+    //Atalho psvm + tab
+    public static <bollean> void main(String[] args) {
+        //int, double, float, char, byte, short, long, boolean
+        // [todos são palavras reservadas e precisam ser escritos com letra minúsculas]
+
+        //int: para números inteiros
+        //double: para números quebrados
+        //String: diferente do char não tem limite de frase
+
+        int idade = (int) 100000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble= 2000.80;
+        float salarioFloat= 2500.80F;
+        byte idadeByte= -127;
+        short idadeHort= 32000;
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+
+        System.out.println("A idade é " + idade + "anos");
+        System.out.println(falso);
+        System.out.println(numeroGrande);
+
+        //String
+        char caractere = 'N';
+        String nome= "Nayara";
+
+        System.out.println(nome);
+
+        //Para concatenar
+        System.out.println("Meu nome é " + nome);
+        System.out.println("char " + caractere);
+
+    }
+}
---
+++ src/MaratonaDojo/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,27 @@
+package MaratonaDojo;
+/*
+EXERCÍCIO:
+
+Criar variáveis para os campos descritos abaixo entre<> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereco>, confirmo que recebi o salário de <salario>, na data <data>.
+ */
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+
+        String nome = "Nayara";
+        String endereco = "R: 9, N°400 - Parque Cidade de Campinas - Campinas - SP";
+        double salario = 2000.00;
+        String dataRecebimentoSalario = "05/06/2024";
+
+        //Imprimindo mensagem com valores
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + dataRecebimentoSalario);
+
+        //Outra forma de fazer é declarar uma variável e imprimir
+        String relatorio = "Eu " + nome + ", morando no endereço " + endereco + ", confirmo que recebi o salário de " + salario + ", na data " + dataRecebimentoSalario;
+        System.out.println(relatorio);
+
+    }
+
+}
