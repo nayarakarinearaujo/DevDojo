18669ebc8e080449dd5ca1f0b9f16c0e904760d1
Nayara <nayarakarine.araujo@gmail.com>
2024-06-12 20:46:44 -0300

    Merge remote-tracking branch 'origin/master/aula12/06/24'

--- src/MaratonaDojo/Aula05EstruturasCondicionais02.java
+++ src/MaratonaDojo/Aula05EstruturasCondicionais02.java
@@ -5,7 +5,7 @@ public class Aula05EstruturasCondicionais02 {
     public static void main(String[] args) {
         //idade < 15 categoria infantil
         //idade >= 15 && idade < 18 categoria juvenil
-        //idade >= categoria adulto
+        //idade >= 18 categoria adulto
 
         int idade = 18;
         String categoria;
@@ -20,7 +20,7 @@ public class Aula05EstruturasCondicionais02 {
 //        }
 
 
-        //Para simplificar o código:
+        //Para simplificar o código e declarar categoria com variável:
 
         if (idade < 15) {
             categoria = "Categoria Infantil";
