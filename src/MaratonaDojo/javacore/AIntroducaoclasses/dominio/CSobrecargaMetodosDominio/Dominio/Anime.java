45962f1a25a5a91172814bc273f5b1950f1fffaa
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-19 10:56:14 -0300

    Aula dia 19/08 - Construtores

--- src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Dominio/Anime.java
+++ src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Dominio/Anime.java
@@ -2,23 +2,45 @@ package MaratonaDojo.javacore.AIntroducaoclasses.dominio.CSobrecargaMetodosDomin
 
 public class Anime {
 
+    private String nome;
     private String tipo;
     private int episodios;
+    private String genero;
 
-
-    public void setTipo(String tipo) {
+    //Método set resumido
+    public void init(String nome, String tipo, int episodios) {
+        this.nome = nome;
         this.tipo = tipo;
+        this.episodios = episodios;
+    }
+
+    //Sobrecarga de métodos
+    public void init(String nome, String tipo, int episodios, String genero) {
+        this.init(nome, tipo, episodios);
+        this.genero = genero;
+    }
+
+    public void imprime() {
+        System.out.println(this.nome);
+        System.out.println(this.tipo);
+        System.out.println(this.episodios);
+        System.out.println(this.genero);
+    }
+
+    public String getNome() {
+        return this.nome;
     }
 
     public String getTipo() {
         return this.tipo;
     }
 
-    public void setEpisodios(int episodios) {
-        this.episodios = episodios;
+    public int getEpisodios() {
+        return this.episodios;
     }
 
-    public int getEpisodios() {
-        return  this.episodios;
+    public String getGenero() {
+        return this.genero;
     }
+
 }
--- src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Teste/AnimeTeste01.java
+++ src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Teste/AnimeTeste01.java
@@ -1,4 +1,13 @@
 package MaratonaDojo.javacore.AIntroducaoclasses.dominio.CSobrecargaMetodosDominio.Teste;
 
+import MaratonaDojo.javacore.AIntroducaoclasses.dominio.CSobrecargaMetodosDominio.Dominio.Anime;
+
 public class AnimeTeste01 {
+    public static void main(String[] args) {
+
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "IV", 12, "Ação");
+        anime.imprime();
+    }
 }
+
