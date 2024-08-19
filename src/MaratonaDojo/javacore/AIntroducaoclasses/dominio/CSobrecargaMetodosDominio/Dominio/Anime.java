1ddfeab6fe52cdc1ced90128c9b9df1f336beea6
Nayara Karine <149000384+nayarakarinearaujo@users.noreply.github.com>
2024-08-19 10:23:22 -0300

    Aula dia 19/08 - Sobrecarga

---
+++ src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Dominio/Anime.java
@@ -0,0 +1,24 @@
+package MaratonaDojo.javacore.AIntroducaoclasses.dominio.CSobrecargaMetodosDominio.Dominio;
+
+public class Anime {
+
+    private String tipo;
+    private int episodios;
+
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return this.tipo;
+    }
+
+    public void setEpisodios(int episodios) {
+        this.episodios = episodios;
+    }
+
+    public int getEpisodios() {
+        return  this.episodios;
+    }
+}
---
+++ src/MaratonaDojo/javacore/AIntroducaoclasses/dominio/CSobrecargaMetodosDominio/Teste/AnimeTeste01.java
@@ -0,0 +1,4 @@
+package MaratonaDojo.javacore.AIntroducaoclasses.dominio.CSobrecargaMetodosDominio.Teste;
+
+public class AnimeTeste01 {
+}
